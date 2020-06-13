655449cd2113ee5cc731a492776a651746bc4f5e
Ethan Brooks <ethanabrooks@gmail.com>
2020-06-13 11:50:02 -0400

    got getScaleTones to work!

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   ReactNativeSVGContext
 } from "standalone-vexflow-context";
 
-type Note = [string, "sharp" | "flat" | null];
+type Note = string;
 type PatternData = { name: string; pattern: number[]; roots: Note[] };
 type Root = number;
 type State =
@@ -17,16 +17,16 @@ type State =
   | { type: "error"; message: string }
   | {
       type: "selectPattern";
-      patterns: List<PatternData>;
+      patterns: PatternData[];
       firstPattern: PatternData;
     }
   | { type: "selectRoot"; pattern: PatternData }
-  | { type: "display"; pattern: PatternData; root: Root };
+  | { type: "display"; notes: Note[] };
 
 export default function App() {
   const [state, setState] = React.useState<State>({ type: "loading" });
   const [pattern, setPattern] = React.useState<PatternData | null>(null);
-  const [root, setRoot] = React.useState<string>("A");
+  const [root, setRoot] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     let validate = ajv.compile(schema);
@@ -37,32 +37,13 @@ export default function App() {
         message: ajv.errorsText(validate.errors)
       });
     } else {
-      const patternList: List<PatternData> = List(
-        patterns.map(pattern => {
-          return {
-            ...pattern,
-            roots: pattern.roots.map(
-              (r: string): Note => {
-                switch (r[1]) {
-                  case "#":
-                    return [r[0], "sharp"];
-                  case "b":
-                    return [r[0], "flat"];
-                  default:
-                    return [r[0], null];
-                }
-              }
-            )
-          };
-        })
-      );
-      const firstPattern: null | PatternData = patternList.first(null);
+      const firstPattern: null | PatternData = List(patterns).first(null);
       if (!firstPattern) {
         setState({ type: "error", message: "patterns were empty" });
       } else {
         setState({
           type: "selectPattern",
-          patterns: patternList,
+          patterns: patterns,
           firstPattern: firstPattern
         });
       }
@@ -87,13 +68,11 @@ export default function App() {
           <Picker
             selectedValue={state.firstPattern}
             style={styles.picker}
-            onValueChange={(value, itemIndex) => setPattern(value)}
+            onValueChange={value => setPattern(value)}
           >
-            {state.patterns
-              .map((p: PatternData) => (
-                <Picker.Item label={p.name} value={p} key={p.name} />
-              ))
-              .toArray()}
+            {state.patterns.map((p: PatternData) => (
+              <Picker.Item label={p.name} value={p} key={p.name} />
+            ))}
           </Picker>
           <Button
             title="Select Scale"
@@ -107,28 +86,26 @@ export default function App() {
         </View>
       );
     case "selectRoot":
+      const music = new Vex.Flow.Music();
+      let keyValue = music.getNoteValue(
+        root || List(state.pattern.roots).first()
+      );
+      const scaleTones: number[] = music.getScaleTones(
+        // @ts-ignore
+        keyValue,
+        state.pattern.pattern
+      );
+      const notes = scaleTones.map(t => music.getCanonicalNoteName(t));
       return (
         <View style={styles.container}>
           <Picker
             selectedValue={root}
             style={styles.picker}
-            onValueChange={(value, itemIndex) => setRoot(value)}
+            onValueChange={setRoot}
           >
-            {state.pattern.roots.map(note => {
-              const noteName = () => {
-                switch (note[1]) {
-                  case "sharp":
-                    return `${note[0]}♯`;
-                  case "flat":
-                    return `${note[0]}♭`;
-                  default:
-                    return note[0];
-                }
-              };
-              return (
-                <Picker.Item label={noteName()} value={note} key={noteName()} />
-              );
-            })}
+            {state.pattern.roots.map(note => (
+              <Picker.Item label={note} value={note} key={note} />
+            ))}
           </Picker>
           {
             <Button
@@ -136,8 +113,7 @@ export default function App() {
               onPress={() =>
                 setState({
                   type: "display",
-                  pattern: state.pattern,
-                  root: 0
+                  notes: notes
                 })
               }
             />
@@ -145,11 +121,17 @@ export default function App() {
         </View>
       );
     case "display":
-      const context = new ReactNativeSVGContext(NotoFontPack, {
-        width: 400,
-        height: 400
-      });
-      const stave = new Vex.Flow.Stave(100, 150, 200);
+      const _notes = state.notes.map((note: Note) =>
+        new Vex.Flow.StaveNote({
+          clef: "treble",
+          keys: [`${note}/5`],
+          duration: "q"
+        }).getKeys()
+      );
+      const context = new ReactNativeSVGContext(NotoFontPack, styles.svg);
+      const stave: Vex.Flow.Stave = new Vex.Flow.Stave(0, 0, 200);
+      const voice = new Vex.Flow.Voice({ num_beats: 4, beat_value: 4 });
+      // voice.addTickables(_notes);
       stave.setContext(context);
       // @ts-ignore
       stave.setClef("treble");
@@ -157,25 +139,14 @@ export default function App() {
       stave.setTimeSignature("4/4");
       stave.draw();
 
-      return <View>{context.render()}</View>;
+      return (
+        <View>
+          {context.render()}
+          <Text>{`${_notes}`}</Text>
+        </View>
+      );
   }
 }
-
-const commonStyles = StyleSheet.create({
-  full: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%"
-  },
-  cell: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
 
 const styles = StyleSheet.create({
   container: {
@@ -191,20 +162,10 @@ const styles = StyleSheet.create({
     alignSelf: "center"
   },
   error: { flex: 1, justifyContent: "center", alignItems: "center" },
-  examples: {
-    ...commonStyles.full,
-    padding: 5,
-    paddingBottom: 0
-  },
-  example: {
-    paddingBottom: 5,
-    flex: 1,
-    flexDirection: "row"
-  },
-  exampleLeft: {
-    ...commonStyles.cell
-  },
-  exampleRight: {
-    ...commonStyles.cell
+  svg: {
+    height: 400,
+    width: 400,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
