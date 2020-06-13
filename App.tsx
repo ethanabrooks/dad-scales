4a1fdc901ed17f6e60923738c0b70881d0e2d669
Ethan Brooks <ethanabrooks@gmail.com>
2020-06-13 19:05:16 -0400

    init

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import patterns from "./patterns.json";
 import { ajv, schema } from "./schema";
 import Vex from "vexflow";
 import { Music } from "./music";
+import RNPickerSelect from "react-native-picker-select";
 
 type Note = string;
 type Pattern = { name: string; pattern: number[]; roots: Note[] };
@@ -31,137 +32,54 @@ type State =
     };
 
 export default function App() {
-  const [state, setState] = React.useState<State>({ type: "loading" });
-
-  React.useEffect(() => {
-    let validate = ajv.compile(schema);
-    const valid = validate(patterns);
-    if (!valid) {
-      setState({
-        type: "error",
-        message: ajv.errorsText(validate.errors)
-      });
-    } else {
-      const firstPattern: null | Pattern = List(patterns).first(null);
-      if (!firstPattern) {
-        setState({ type: "error", message: "patterns were empty" });
-      } else {
-        setState({
-          type: "selectPattern",
-          patterns: patterns,
-          pattern: firstPattern
-        });
-      }
-    }
-  }, []);
-
-  const errorScreen = (message: string) => (
-    <View style={styles.error}>
-      <Text style={{ fontWeight: "bold" }}>Error!</Text>
-      <Text>🙀</Text>
-      <Text style={{ textAlign: "center" }}>{message}</Text>
-    </View>
+  const [pattern, setPattern] = React.useState<Pattern | null>(null);
+  const [root, setRoot] = React.useState<Note | null>(null);
+  let patternPicker = (
+    <RNPickerSelect
+      placeholder={{
+        label: "Select a scale type...",
+        value: null
+      }}
+      onValueChange={(_, i) => setPattern(patterns[i])}
+      items={patterns.map((p: Pattern) => ({
+        label: p.name,
+        value: p
+      }))}
+    />
   );
-
-  switch (state.type) {
-    case "loading":
-      return <Text style={styles.container}>Loading…</Text>;
-    case "error":
-      return errorScreen(state.message);
-    case "selectPattern":
-      const root = List(state.pattern.roots).first(null);
-      if (!root) {
-        return errorScreen("roots were empty");
-      } else {
-        return (
-          <View style={styles.container}>
-            <Picker
-              selectedValue={state.pattern}
-              style={styles.picker}
-              onValueChange={value =>
-                setState({
-                  ...state,
-                  pattern: value
-                })
-              }
-            >
-              {state.patterns.map((p: Pattern) => (
-                <Picker.Item label={p.name} value={p} key={p.name} />
-              ))}
-            </Picker>
-            {
-              <Button
-                title="Select Scale"
-                onPress={() =>
-                  setState({
-                    ...state,
-                    type: "selectRoot",
-                    root: root
-                  })
-                }
-              />
-            }
-          </View>
-        );
-      }
-    case "selectRoot":
+  const rootPicker =
+    pattern && pattern.roots ? (
+      <RNPickerSelect
+        placeholder={{
+          label: "Select a scale root...",
+          value: null
+        }}
+        onValueChange={(_, i) => setRoot(pattern.roots[i])}
+        items={pattern.roots.map(note => ({ label: note, value: note }))}
+      />
+    ) : null;
+  const sheetmusic = () => {
+    if (pattern && root) {
       const music = new Vex.Flow.Music();
       const scaleTones: number[] = music.getScaleTones(
         // @ts-ignore
-        music.getNoteValue(state.root),
-        state.pattern.pattern
+        music.getNoteValue(root),
+        pattern
       );
       const notes = scaleTones.map(t => music.getCanonicalNoteName(t));
-      return (
-        <View style={styles.container}>
-          <Picker
-            selectedValue={state.root}
-            style={styles.picker}
-            onValueChange={value => setState({ ...state, root: value })}
-          >
-            {state.pattern.roots.map(note => (
-              <Picker.Item label={note} value={note} key={note} />
-            ))}
-          </Picker>
-          <View style={styles.buttons}>
-            <Button
-              title="Select Scale"
-              onPress={() =>
-                setState({
-                  ...state,
-                  type: "display",
-                  notes: notes
-                })
-              }
-            />
-            <Button
-              title="Back"
-              onPress={() =>
-                setState({
-                  ...state,
-                  type: "selectPattern"
-                })
-              }
-            />
-          </View>
-        </View>
-      );
-    case "display":
-      return (
-        <View>
-          {new Music(state.notes, styles.svg).render()}
-          <Button
-            title="Back"
-            onPress={() =>
-              setState({
-                ...state,
-                type: "selectRoot"
-              })
-            }
-          />
-        </View>
-      );
-  }
+      return new Music(notes, styles.svg).render();
+    }
+  };
+  let message = sheetmusic();
+  console.log(message);
+
+  return (
+    <View style={styles.container}>
+      {patternPicker}
+      {rootPicker}
+      {message}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
